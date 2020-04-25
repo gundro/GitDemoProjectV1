f2f3191e46fd204c092d11628bbd94f7108af7f0
ronithrajgund <ronithrajgund@192.168.0.192>
2020-04-25 15:48:17 +0530

    First Commit by Lead

---
+++ src/gitDemo/Test.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package gitDemo;
+
+/**
+ * @author ronithrajgund
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String hungry="yes";
+		
+		if("yes" == hungry)
+		{
+			System.out.println("Eat Something");
+		}
+		else
+		{
+			System.out.println("Do work");
+		}
+	}
+
+}
